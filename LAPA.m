40c0ae0f0b925cd04f5eee5f9a5d49bdcdebc30e
Jay Maini <jaymaini@cmail.carleton.ca>
2021-02-03 11:44:08 -0500

    Final PA version

--- LAPA.m
+++ LAPA.m
@@ -1,28 +1,46 @@
-set(0,'DefaultFigureWindowStyle','docked')
+%Jay Maini 101037537
+%PA 4 - Finite Difference Formulation
 
-nx = 100;
-ny = 100;
-ni = 10000;
+set(0,'DefaultFigureWindowStyle','docked')
+close all;
+
+nx = 50;
+ny = 50;
+ni = 2500;
 V = zeros(nx,ny);
 
 SidesToZero = 1;
 for k=1:ni
-    for i=1:nx
-        for j=1:ny
+    for m=1:nx
+        for n=1:ny
             %Something here
             
             %Set/Reset boundary conditions
-            %1 on left side
-            
-            %0 on right side 
+            if (n == 1)
+                %Edge case: V = 1 on left side
+                V(m,n) = 1;
+            elseif (n == ny)
+                %Edge case: V = 0 on right side
+                V(m,n) = 0;    
+            elseif (m == 1)
+                %Bottom edge case: Only affected by 3 surrounding particles
+                V(m,n) = (V(m+1,n)+V(m,n+1)+V(m,n-1))/3;    
+            elseif (m == nx) 
+                %Top edge case: Only affected by 3 surrounding particles
+                V(m,n) = (V(m-1,n)+V(m,n+1)+V(m,n-1))/3;     
+            else
+                %Full finite-difference formulation
+                 V(m,n) = (V(m+1,n)+V(m-1,n)+V(m,n+1)+V(m,n-1))/4;
+            end
+        
         end
         
         if mod(k,50) == 0
-            surf(V;)
-            pause(0.05)
+            surf(V)
+            pause(0.01)
         end
     end
-
+end
 [Ex, Ey] = gradient(V);
 
 figure
